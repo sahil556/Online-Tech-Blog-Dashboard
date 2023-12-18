fc166ed51e025480f9ff3bc9b413bca6318132f6
sahil556 <sahilnandaniya556@gmail.com>
2023-12-18 14:51:08 +0530

    Add Category form created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+
+import { environment } from 'src/environments/environment.prod';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoryComponent } from './category/category.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyARXs9mTR4Nia2qM_7BFrD7Pi7IdFaKy9g",
+        authDomain: "tech-blog-application.firebaseapp.com",
+        projectId: "tech-blog-application",
+        storageBucket: "tech-blog-application.appspot.com",
+        messagingSenderId: "462185746623",
+        appId: "1:462185746623:web:7c0d6137c12b604cb7f60a"
+    }
+}
